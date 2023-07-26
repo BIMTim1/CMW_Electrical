5456d26d5d1f6fa1e3dcb337705e0b99112edf7d
BIMTim <tim.ingle@cmta.com>
2023-07-26 08:06:52 -0500

    Added initial element collection; added first failure test.

--- CMW_Electrical/MotorMOCPUpdate/UpdateMotorMOCP.cs
+++ CMW_Electrical/MotorMOCPUpdate/UpdateMotorMOCP.cs
@@ -27,7 +27,31 @@ namespace MotorMOCPUpdate
             Application app = uiapp.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
 
+            //define BuiltInCategory to collect
+            BuiltInCategory bic = BuiltInCategory.OST_ElectricalFixtures;
+
+            //collect all Motors in model
+            List<Element> all_motors = new FilteredElementCollector(doc)
+                .OfCategory(bic)
+                .WhereElementIsNotElementType()
+                .ToElements()
+                .Where(x=>x.LookupParameter("Family").AsValueString().Contains("Motor"))
+                .ToList();
+
+            //check if the tool collected any Motor Elements
+            //if no, close the tool.
+            if (all_motors.Count == 0)
+            {
+                TaskDialog.Show("No Motors in Project", "There are no Motor Families placed in the Active Project. The tool will now close.");
+                return Result.Failed;
+            }
+
             return Result.Succeeded;
+        }
+
+        public bool CorrectMOCP(Element mtr, Document document)
+        {
+            return false;
         }
     }
 }
