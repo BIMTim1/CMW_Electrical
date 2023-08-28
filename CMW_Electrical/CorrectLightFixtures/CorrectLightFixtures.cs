41e94c05133edfb78526c88033b57dadefca3ca9
BIMTim <tim.ingle@cmta.com>
2023-08-28 17:02:19 -0500

    Completed through line 74 of E_CorrectLightFixtures.py script. Added collections, FindFieldByName method, and UpdateParameterInfo method.

--- CMW_Electrical/CorrectLightFixtures/CorrectLightFixtures.cs
+++ CMW_Electrical/CorrectLightFixtures/CorrectLightFixtures.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,7 +17,100 @@ namespace CorrectLightFixtures
     {
         public Result Execute(ExternalCommandData commandData, ref string errorReport, ElementSet elementSet)
         {
+            //define background Revit information to reference
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            Application app = uiapp.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+            //collect default file information
+            BuiltInCategory LTGBIC = BuiltInCategory.OST_LightingFixtures;
+
+            const string OLD_PATH = "V:/Content/CMS/Lighting Fixtures/221019_Old Lighting Fixtures";
+            const string NEW_PATH = "V:/Content/CMS/Lighting Fixtures";
+
+            const string PARAM_TEST = "Light Fixture Schedule_ML-Manufacturer";
+
+            //default parameters
+            List<string> new_string_parameters = new List<string>()
+            {
+                "Light Fixture Schedule_ML-Manufacturer", "Light Fixture Schedule_ML-Series",
+                "Light Fixture Schedule_ML-CRI", "Light Fixture Schedule_ML-Lumens",
+                "Light Fixture Schedule_ML-Dimming", "Light Fixture Schedule_ML-Mounting",
+                "Light Fixture Schedule_ML-Finish", "Light Fixture Schedule_ML-Dimensions"
+            };
+
+            List<string> old_string_parameters = new List<string>()
+            {
+                "Light Fixture Schedule_Manufacturer", "Light Fixture Schedule_Series",
+                "Light Fixture Schedule_Lamp CRI", "Light Fixture Schedule_Lamp Lumens",
+                "Light Fixture Schedule_Ballast Starting Notes", "Light Fixture Schedule_Mounting",
+                "Light Fixture Schedule_Finish", "Light Fixture Schedule_Dimensions"
+            };
+
+            List<string> same_double_parameters = new List<string>()
+            {
+                "Light Fixture Schedule_Voltage", "Light Fixture Schedule_Apparent Load"
+            };
+
+            List<string> allFixtureFamilyNames = (from fixture in 
+                                                      new FilteredElementCollector(doc)
+                                                      .OfClass(typeof(Family))
+                                                      .Cast<Family>()
+                                                      .Where(x => x.FamilyCategory.Name.ToString() == "Lighting Fixture")
+                                                      .ToList() 
+                                                  select fixture.Name.ToString())
+                                                  .ToList();
+
+            List<Element> lightingSchedules = new FilteredElementCollector(doc)
+                .OfClass(typeof(ViewSchedule))
+                .ToElements()
+                .Where(x => x.Name != null && x.Name.ToString().Contains("LIGHT FIXTURE SCHEDULE"))
+                .ToList();
+
+            Transaction trac = new Transaction(doc);
+
             return Result.Succeeded;
+        }
+
+        public bool FindFieldByName(ScheduleDefinition schedDef, string fieldName, Document document)
+        {
+            bool returnBool = false;
+
+            try
+            {
+                IList<ScheduleFieldId> fieldIds = schedDef.GetFieldOrder();
+
+                foreach (ScheduleFieldId fieldId in fieldIds)
+                {
+                    if (schedDef.GetField(fieldId).GetSchedulableField().GetName(document) == fieldName)
+                    {
+                        returnBool = true;
+                    }
+                }
+            }
+            catch
+            {
+                returnBool = false;
+            }
+
+            return returnBool;
+        }
+
+        public void UpdateParameterInfo(string updateInfo, FamilySymbol oldFixtureType, 
+            FamilySymbol newFixtureType, List<string> newParams, 
+            List<string> oldParams, List<string> integerParams)
+        {
+            List<string[]> zippedList = new List<string[]>();
+
+            if (updateInfo == "old")
+            {
+                zippedList = oldParams.Zip(newParams, (oldP, newP) => new[] { oldP, newP }).ToList();
+            }
+            else
+            {
+                zippedList = newParams.Zip(oldParams, (newP, oldP) => new[] { newP, oldP }).ToList();
+            }
         }
     }
 }
