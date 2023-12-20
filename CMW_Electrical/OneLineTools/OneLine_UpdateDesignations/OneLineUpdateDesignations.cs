3d0b066d45503ba6353910bfc26d5486e93ac585
BIMTim <tim.ingle@cmta.com>
2023-12-20 17:38:09 -0600

    Add DetailComponent and ElectricalEquipment FilteredElementCollectors to OneLineUpdateDesignations.

--- CMW_Electrical/OneLineTools/OneLine_UpdateDesignations/OneLineUpdateDesignations.cs
+++ CMW_Electrical/OneLineTools/OneLine_UpdateDesignations/OneLineUpdateDesignations.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,27 @@ namespace OneLineUpdateDesignations
     {
         public Result Execute(ExternalCommandData commandData, ref string errorReport, ElementSet elementSet)
         {
+            //define background Revit information to reference
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            Application app = uiapp.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+            //collect Detail Items and Electrical Equipment with an EqConId
+            List<FamilyInstance> allDetailItems = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DetailComponents)
+                .OfClass(typeof(FamilyInstance))
+                .Where(x => x.LookupParameter("EqConId").AsString() != "")
+                .Cast<FamilyInstance>()
+                .ToList();
+
+            List<FamilyInstance> allElectricalEquip = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_ElectricalEquipment)
+                .OfClass(typeof(FamilyInstance))
+                .Where(x => x.LookupParameter("EqConId").AsString() != "")
+                .Cast<FamilyInstance>()
+                .ToList();
+
             //create an instance of the WindowsForm for the user to select which update method to use
             dialogSelectUpdateMethod dialogSelectUpdateMethod = new dialogSelectUpdateMethod();
             dialogSelectUpdateMethod.ShowDialog();
