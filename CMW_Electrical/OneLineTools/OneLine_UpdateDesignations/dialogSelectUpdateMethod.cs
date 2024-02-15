b9163c33f0e8b59e06f0e70fe4b7727b9ba61e57
BIMTim <tim.ingle@cmta.com>
2024-02-15 08:01:33 -0600

    Updated forms to have rounded edges and green label at top.

--- CMW_Electrical/OneLineTools/OneLine_UpdateDesignations/dialogSelectUpdateMethod.cs
+++ CMW_Electrical/OneLineTools/OneLine_UpdateDesignations/dialogSelectUpdateMethod.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Autodesk.Revit.DB.Macros;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +15,21 @@ namespace OneLineUpdateDesignations
 {
     public partial class dialogSelectUpdateMethod : System.Windows.Forms.Form
     {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+            (
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+            );
         public dialogSelectUpdateMethod()
         {
             InitializeComponent();
+
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
 
             ToolTip useEquipToolTip = new ToolTip();
             useEquipToolTip.SetToolTip(rbtnUseEquipment, "This option is used to update all Detail Item families in the One-Line / Riser schematic that have the same EqConId value as an Electrical Equipment family located in the Revit model.");
