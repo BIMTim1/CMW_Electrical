85fa96da5a75a829e63f3ac4b71aa4f706a35c45
BIMTim1 <tim.ingle@cmta.com>
2024-03-20 17:32:01 -0500

    Add resource images; created initial folder structure and classes; created GetScheduleFormatting general class and initial logic.

---
+++ CMW_Electrical/PanelSchedFormatting/SpareAndSpaceAlign/SpareAndSpaceAlign.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB.Electrical;
+
+namespace PanelSchedFormatting
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    internal class SpareAndSpaceAlign : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string errorReport, ElementSet elementSet)
+        {
+            //define background Revit information to reference
+            UIApplication uiapp = commandData.Application;
+            Document doc = uiapp.ActiveUIDocument.Document;
+            Application app = uiapp.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+
+
+            return Result.Succeeded;
+        }
+    }
+}
