05a00828b6eb0232069df92208d7576b0bde681d
BIMTim1 <tim.ingle@cmta.com>
2024-03-21 16:02:46 -0500

    Updated PanelSchedFinalFormat and SpareAndSpaceAlign to contain logic from python source.

--- CMW_Electrical/PanelSchedFormatting/SpareAndSpaceAlign/SpareAndSpaceAlign.cs
+++ CMW_Electrical/PanelSchedFormatting/SpareAndSpaceAlign/SpareAndSpaceAlign.cs
@@ -24,8 +24,86 @@ namespace PanelSchedFormatting
             Application app = uiapp.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
 
+            //collect all panelboard schedules in project
+            List<PanelScheduleView> allPanelSchedules = 
+                new FilteredElementCollector(doc)
+                .OfClass(typeof(PanelScheduleView))
+                .ToElements()
+                .Cast<PanelScheduleView>()
+                .ToList();
 
-            return Result.Succeeded;
+            if (!allPanelSchedules.Any())
+            {
+                TaskDialog.Show("No Panelboard Schedules", "There are no Panelboard schedules created in this project. The tool will now cancel.");
+
+                return Result.Cancelled;
+            }
+
+            using (Transaction trac = new Transaction(doc))
+            {
+                try
+                {
+                    trac.Start("Spare and Space Alignment");
+
+                    foreach (PanelScheduleView panSchedView in allPanelSchedules)
+                    {
+                        //collect PanelScheduelView Body TableSectionData
+                        TableData tableData = panSchedView.GetTableData();
+                        TableSectionData sectionData = tableData.GetSectionData(SectionType.Body);
+
+                        //collect PanelScheduleTemplate to determine rows and columns to itereate through
+                        PanelScheduleType panSchedType = (doc.GetElement(panSchedView.GetTemplate()) as PanelScheduleTemplate).GetPanelScheduleType();
+                        
+                        //collect column and row information 
+                        GetScheduleFormatting schedFormat = new GetScheduleFormatting();
+                        List<Int32> columns = schedFormat.GetPanelScheduleColumns(panSchedType, panSchedView, doc);
+                        Int32 rows = schedFormat.GetPanelScheduleRows(panSchedType, panSchedView);
+
+                        //iterate through columns and rows of PanelScheduleView
+                        for (Int32 rowNum = 2; rowNum <= rows; rowNum++)
+                        {
+                            foreach (Int32 colNum in columns)
+                            {
+                                //check if Slot is Spare or Space
+                                bool isSpare = panSchedView.IsSpare(rowNum, colNum);
+                                bool isSpace = panSchedView.IsSpace(rowNum, colNum);
+
+                                //collect TableCellStyle information to update with FontHorizontalAlignment determined by Spare or Space
+                                TableCellStyle cellStyle = sectionData.GetTableCellStyle(rowNum, colNum);
+
+                                HorizontalAlignmentStyle horizAlignment = new HorizontalAlignmentStyle();
+
+                                if (isSpare || isSpace)
+                                {
+                                    if (isSpare)
+                                    {
+                                        horizAlignment = HorizontalAlignmentStyle.Right;
+                                    }
+                                    else if (isSpace)
+                                    {
+                                        horizAlignment = HorizontalAlignmentStyle.Right;
+                                    }
+
+                                    cellStyle.FontHorizontalAlignment = horizAlignment;
+
+                                    sectionData.SetCellStyle(rowNum, colNum, cellStyle);
+                                }
+
+                            }
+                        }
+                    }
+
+                    trac.Commit();
+
+                    return Result.Succeeded;
+                }
+                catch (Exception ex)
+                {
+                    TaskDialog.Show("An error occurred", "An error occurred that has stopped the tool. Contact the BIM team for assistance.");
+
+                    return Result.Failed;
+                }
+            }
         }
     }
 }
