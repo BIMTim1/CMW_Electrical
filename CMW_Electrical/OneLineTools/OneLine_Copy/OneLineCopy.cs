a4758da267c9d56be0a919339e8426b410df7b69
BIMTim1 <tim.ingle@cmta.com>
2024-05-10 10:36:43 -0500

    Deleted OneLine_TrimExtend folder and class, tool would not be useful due to how built in Revit tools work with trimming and extending Line Based Detail Item families; added functionality to OneLineCopy.

--- CMW_Electrical/OneLineTools/OneLine_Copy/OneLineCopy.cs
+++ CMW_Electrical/OneLineTools/OneLine_Copy/OneLineCopy.cs
@@ -28,7 +28,62 @@ namespace OneLineCopy
             Application app = uiapp.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
 
+            View activeView = doc.ActiveView;
+
+            if (activeView.ViewType != ViewType.DraftingView)
+            {
+                TaskDialog.Show("Incorrect View Type",
+                    "This tool can only be run in a Drafting View. Change the current view to a Drafting View and rerun the tool.");
+                return Result.Cancelled;
+            }
+
+            ICollection<ElementId> selectedIds = uidoc.Selection.GetElementIds();
+            ICollection<Element> selectedElems = new List<Element>();
+
+            if (selectedIds.Count() == 0)
+            {
+                try
+                {
+                    //ISelectionFilter selFilter = new DetailItemSelectionFilter();
+                    //selectedElems = uidoc.Selection.PickElementsByRectangle(selFilter, "Select One Line elements to copy.");
+                    selectedElems = uidoc.Selection.PickElementsByRectangle("Select One Line elements to copy."); //debug only
+                }
+                catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+                {
+                    return Result.Cancelled;
+                }
+                catch (Exception ex)
+                {
+                    TaskDialog.Show("Error occurred", "An error occurred that has prevented the tool from running. Contact the BIM team for assistance.");
+                    return Result.Failed;
+                }
+            }
+            else
+            {
+                foreach (ElementId elemId in selectedIds)
+                {
+                    Element elem = doc.GetElement(elemId);
+                    selectedElems.Add(elem);
+                }
+            }
+
             return Result.Succeeded;
+        }
+
+        public class DetailItemSelectionFilter : ISelectionFilter
+        {
+            public bool AllowElement(Element element)
+            {
+                if (element.Category.Name == "Detail Items")
+                {
+                    return true;
+                }
+                return false;
+            }
+            public bool AllowReference(Reference refer, XYZ point)
+            {
+                return false;
+            }
         }
     }
 }
