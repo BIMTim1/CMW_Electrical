8b51fd123cdffbb26b8d99f2fd75af3159fe0385
BIMTim1 <tim.ingle@cmta.com>
2024-09-10 10:26:45 -0500

    Created and debugged UpdateNestedFamilyFont.cs.

--- CMW_Electrical/CMW_Electrical_Ribbon.cs
+++ CMW_Electrical/CMW_Electrical_Ribbon.cs
@@ -728,6 +728,24 @@ namespace CMW_Electrical
 
             PushButton getCircuitsPlaceTextBtn = circuitPanel.AddItem(getCircuitsPlaceTextData) as PushButton;
             //set ContextualHelp for PushButton
+
+
+            PushButtonData nestedFamilyFontUpdateData = new PushButtonData(
+                "updateNestedFamilyFontButton", 
+                "Font" + System.Environment.NewLine + " Refresh ", 
+                thisAssemblyPath, 
+                "NestedFontUpdate.UpdateNestedFamilyFont")
+            {
+                LargeImage = new BitmapImage(new Uri("pack://application:,,,/CMW_Electrical;component/Resources/NestedFontUpdate32x32.png")),
+                Image = new BitmapImage(new Uri("pack://application:,,,/CMW_Electrical;component/Resources/NestedFontUpdate16x16.png")),
+                ToolTip = "Update the nested Font of a Family.",
+                LongDescription = "This tool will update the nested annotation font of a selected family to be default ARchitxt. " +
+                "This tool helps speed up the process of correcting families from an error that can occur after " +
+                "a project is upgraded to a later version of Revit."
+            };
+
+            PushButton nestedFamilyFontUpdateBtn = devicePanel.AddItem(nestedFamilyFontUpdateData) as PushButton;
+            //set ContextualHelp for PushButton
         }
 
         public Result OnShutdown(UIControlledApplication application)
